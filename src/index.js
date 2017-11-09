9e46b5ffe0305bf36d377cf154bc6947fd0ddd15
netops-PC\Vipul Dholaria <vipul.dholariya@tarento.com>
2017-11-09 17:53:30 +0530

    Lifecycle- 11

--- src/index.js
+++ src/index.js
@@ -4,103 +4,42 @@ import './index.css';
 import TodoItem from './components/TodoItem';
 import TodoForm from "./components/TodoForm";
 
-class TodoList extends React.Component {
-    constructor() {
-        super();
-        this.changeStatus = this.changeStatus.bind(this);
-        this.updateTask = this.updateTask.bind(this);
-        this.addTask = this.addTask.bind(this);
-        this.deleteTask = this.deleteTask.bind(this);
-        this.editTask = this.editTask.bind(this);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
         this.state = {
-         tasks:[
-             {
-                 name:"Buy Milk",
-                 completed: false
-             },
-             {
-                 name:"Buy Cheeps",
-                 completed: false
-             },
-             {
-                 name:"Buy Butter",
-                 completed: false
-             }
-         ],
-            currentTask : ''
+            count:0
         }
+        console.log("constructor")
     }
-    changeStatus(index) {
-        var tasks = this.state.tasks;
-        var task = tasks[index];
-        task.completed = !task.completed;
+    incCount = () => {
         this.setState({
-            tasks:tasks
+            count:this.state.count+1
         })
     }
-    editTask(index, newValue) {
-        var tasks = this.state.tasks;
-        var task = tasks[index];
-        task['name'] = newValue;
-
-        this.setState({
-            tasks:tasks
-        })
+    shouldComponentUpdate() {
+        if(this.state.count >= 10){
+            return false;
+        }
+        return true;
     }
-    updateTask(newValue) {
-        this.setState({
-            currentTask: newValue.target.value
-        })
+    componentWillMount(){
+        console.log("will mount");
     }
-    deleteTask(index) {
-        let tasks = this.state.tasks;
-        tasks.splice(index, 1);
-
-        this.setState({
-            tasks
-        })
+    componentDidMount(){
+        console.log("Did Mount")
     }
-    addTask(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        let tasks = this.state.tasks;
-        let currentTask = this.state.currentTask;
-        tasks.push({
-            name: currentTask,
-            completed: false
-        });
-
-        this.setState({
-            tasks
-        })
-    }
-
     render(){
+        console.log("rendering");
         return (
             <section>
-                <TodoForm
-                    currentTask = {this.state.currentTask}
-                    updateTask = {this.updateTask}
-                    addTask = {this.addTask}
-                />
-                <ul>
-                    {
-                        this.state.tasks.map((task, index) => {
-                            return <TodoItem
-                                key={index}
-                                index={index}
-                                deleteTask={this.deleteTask}
-                                editTask={this.editTask}
-                                clickHandler={this.changeStatus}
-                                detail={task}
-                            />
-                        })
-                    }
-                </ul>
+                {this.state.count}
+                <button onClick={this.incCount}>Increment</button>
             </section>
         )
     }
 }
 
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
